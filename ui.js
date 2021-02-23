d7bd314e3597f9452c31d17ad8dfaf3f3a53616b
Peter Gregory <prgregory97@gmail.com>
2021-02-23 12:03:47 +0000

    colortable!

--- ui.js
+++ ui.js
@@ -9,18 +9,24 @@ function generate_table() {
     var tblBody = document.createElement("tbody");
   
     // creating all cells
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 100; i++) {
       // creates a table row
       var row = document.createElement("tr");
   
-      for (var j = 0; j < 10; j++) {
+      for (var j = 0; j < 100; j++) {
         // Create a <td> element and a text node, make the text
         // node the contents of the <td>, and put the <td> at
         // the end of the table row
         var cell = document.createElement("td");
-        var cellText = document.createTextNode("row "+i+", column "+j);
-        cell.appendChild(cellText);
+        // var cellText = document.createTextNode("row "+i+", column "+j);
+        // var cellText = document.createTextNode(0);
+        // cell.appendChild(cellText);
         row.appendChild(cell);
+        if ((i % 2 == 0 && j % 2 ==0) || (i % 2 != 0 && j % 2 != 0)) {
+            cell.style.backgroundColor = 'black';
+        }
+        
+        // console.log(i,j)
       }
   
       // add the row to the end of the table body
@@ -31,6 +37,4 @@ function generate_table() {
     tbl.appendChild(tblBody);
     // appends <table> into <body>
     body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
-    tbl.setAttribute("border", "1");
   }
